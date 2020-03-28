8b988023a36088189fb29c435266505c06ba8cc9
louis-j <czzcy3832515@gmail.com>
2020-03-28 18:09:09 +0800

    更新bdwgc，编译了clang的版本，现所有示例和测试均可在gnu/clang双平台编译运行

--- test/0/gc_cpp.h
+++ test/0/gc_cpp.h
@@ -194,7 +194,7 @@ by UseGC.  GC is an alias for UseGC, unless GC_NAME_CONFLICT is defined.
                 do { if (!(obj)) GC_abort_on_oom(); } while (0)
 #elif defined(GC_INCLUDE_NEW)
 # include <new> // for bad_alloc
-# define GC_OP_NEW_OOM_CHECK(obj) if (obj) {} else throw bad_alloc()
+# define GC_OP_NEW_OOM_CHECK(obj) if (obj) {} else throw std::bad_alloc()
 #else
   // "new" header is not included, so bad_alloc cannot be thrown directly.
   GC_API void GC_CALL GC_throw_bad_alloc();
